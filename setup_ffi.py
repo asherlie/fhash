10f3841c515eaa4f456dc7aa3a5bbef88e19a136
asherlie <lieber.asher@gmail.com>
2022-12-27 22:50:07 -0500

    updated setup_ffi.py to use new pyffi.c interface

--- setup_ffi.py
+++ setup_ffi.py
@@ -5,14 +5,14 @@ ffibuilder=FFI()
 ffibuilder.set_source(
 "_phash",
 """
-void init(char*, int, int, int, _Bool);
-void build_hdr(char*);
+void init(char* fn);
+void build_hdr(float danceability, float energy, int key, float loudness, float speechiness, float acousticness, char* name);
 void finalize_hdr();
-void insert(char*, int);
+void insert(float danceability, float energy, int key, float loudness, float speechiness, float acousticness, char* name);
 void seal();
-int lookup_quick(char*, char*);
-void load(char*);
-int lookup(char*);
+void load(char* fn);
+char* lookup(float danceability, float energy, int key, float loudness, float speechiness, float acousticness);
+char** lookup_bucket(float danceability, float energy, int key, float loudness, float speechiness, float acousticness);
 """,
 sources = ['phash.c', 'pyffi.c'],
 library_dirs = [],
@@ -20,14 +20,14 @@ libraries = ['atomic'],
 )
 
 ffibuilder.cdef("""
-void init(char*, int, int, int, _Bool);
-void build_hdr(char*);
+void init(char* fn);
+void build_hdr(float danceability, float energy, int key, float loudness, float speechiness, float acousticness, char* name);
 void finalize_hdr();
-void insert(char*, int);
+void insert(float danceability, float energy, int key, float loudness, float speechiness, float acousticness, char* name);
 void seal();
-int lookup_quick(char*, char*);
-void load(char*);
-int lookup(char*);
+void load(char* fn);
+char* lookup(float danceability, float energy, int key, float loudness, float speechiness, float acousticness);
+char** lookup_bucket(float danceability, float energy, int key, float loudness, float speechiness, float acousticness);
 """)
 
 ffibuilder.compile(verbose=True);
